87aa5e7fdd498d3bf24160e948c6753eb5f80185
David <davvblack@gmail.com>
2012-08-25 15:54:23 -0400

    Added basic state controller and lobby menu.

--- ui/js/GONADS-Controllers.js
+++ ui/js/GONADS-Controllers.js
@@ -3,6 +3,66 @@ GONADS.Lobby = Em.Object.create({
 
         }
 
-
-
     })
+
+GONADS.Game = Em.Object.extend({
+    init: function() {
+        GONADS.map = GONADS.Map.create();
+    },
+
+
+    destroy: function() {
+        delete GONADS.map;
+    }
+});
+
+GONADS.Map = Em.Object.extend({
+    init: function() {
+        
+    }
+});
+
+GONADS.Status = Em.StateManager.extend({
+    at: function () {
+        return this.get('currentState.name');
+    }.property('currentState.name'),
+
+    is: function (compare) {
+        return this.get('currentState.name') == compare;
+    },
+
+    has_ancestor: function (compare) {
+        return this.get('currentState.path').split('.').indexOf(compare)!=-1;
+    }
+});
+
+GONADS.GameState = GONADS.Status.create({
+    initialState: 'in_lobby',
+    states: {
+
+        in_lobby: Em.State.create({
+            enter: function () {
+                GONADS.lobby_menu = GONADS.LobbyMenu.create();
+                GONADS.lobby_menu.appendTo("#main");
+            },
+            exit: function () {
+                GONADS.lobby_menu.tear_down(function () {
+                    GONADS.lobby_menu.remove();
+                });
+            }
+        }),
+
+        in_game: Em.State.create({
+            enter: function () {
+                GONADS.game = GONADS.Game.create();
+            },
+            exit: function () {
+                GONADS.game.destroy();
+            }
+        }),
+
+        in_limbo: Em.State.create({
+
+        }),
+    }
+})
---
+++ ui/js/GONADS-Views.js
@@ -0,0 +1,22 @@
+GONADS.View = Em.View.extend();
+
+GONADS.AView = Em.View.extend({
+    didInsertElement: function () {
+        this.$().slideDown(1000);
+    },
+    tear_down: function (callback) {
+        //to_remove=this;
+        //
+        this.$().slideUp(1000, function () {
+            callback();
+        });
+    }
+
+    });
+
+GONADS.LobbyMenu = GONADS.AView.extend({
+    enter_game: function(){
+        GONADS.GameState.goToState('in_game');
+    },
+    templateName: 'lobby-menus'
+})
