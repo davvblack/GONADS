a48127cbbb85a5a9e91a3755d29c80432b379054
David <davvblack@gmail.com>
2012-08-27 15:51:49 -0400

    Best zindexing.

--- ui/js/GONADS-Views.js
+++ ui/js/GONADS-Views.js
@@ -86,7 +86,7 @@ GONADS.EntityArt = GONADS.View.extend({
         //console.log(bl);
         var bl_next = bl_from_xy(this.get('content.x')+coord_delta.x, this.get('content.y')+coord_delta.y);
 //css({left:bl.left+30+'px',bottom:bl.bottom+30+'px'})
-        this.$().css('z-index',-Math.max(bl.bottom,bl_next.bottom)+10);
+        this.$().css('z-index',-Math.min(bl.bottom,bl_next.bottom)-1);
         //console.log(bl.left, bl_next.left);
         this.$().animate({left:bl_next.left+30+'px',bottom:bl_next.bottom+30+'px'}, this.get('content.speed')-300);
         var direction_names = {N:'north',S:'south',E:'east',W:'west'};
